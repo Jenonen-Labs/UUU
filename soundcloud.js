d39872039f26397817e43643102b3520d216746e
Jenonen <167656655+Jenonen-Labs@users.noreply.github.com>
2024-04-21 21:33:55 -0500

    Delete soundcloud.js

--- soundcloud.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'soundcloud',
-    description: "sends the soundcloud link!",
-    execute(message){
-        message.channel.send('https://soundcloud.com/jenonen/tracks')
-    }
-}
